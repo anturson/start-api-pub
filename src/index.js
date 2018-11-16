ab22e0dbed7a928850ae9ff3578eb5d48c33a3ea
Artur Nizamov <artur.nizamov@project-plato.com>
2018-11-16 14:45:11 +0300

    es6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 
-var app = express();
+const app = express();
 
 app.use(cors());
+const port = process.env.PORT || 5000;
+app.set('port', port);
 
-app.set('port', process.env.PORT || 5000);
-
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.get('/users', function (req, res) {
+app.get('/users', (req, res) => {
   res.json([
     { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
     { id: 2, firstName: "Tammy", lastName: "Norton", email: "tammy@gmail.com" },
@@ -19,6 +19,6 @@ app.get('/users', function (req, res) {
   ]);
 });
 
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), () => {
   console.log("Node app is running at localhost: " + app.get('port')); // eslint-disable-line no-console
 });
