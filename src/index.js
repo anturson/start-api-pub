c2fe6e9c9246cc64410f7e1cacec415a7b001b65
Artur Nizamov <artur.nizamov@project-plato.com>
2018-11-16 14:36:31 +0300

    babel-node

--- src/index.js
+++ src/index.js
@@ -20,5 +20,5 @@ app.get('/users', function (req, res) {
 });
 
 app.listen(app.get('port'), function () {
-  console.log("Node app is running at localhost: " + app.get('port'));
+  console.log("Node app is running at localhost: " + app.get('port')); // eslint-disable-line no-console
 });
---
+++ src/index.test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+// import fs from 'fs';
+
+describe.skip('First test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe.skip('index.html', () => {
+  it('should have h1 that says Users', (done) => {
+    // const index = fs.readFileSync('./src/index.html', 'utf-8');
+    // const { window } = new JSDOM(index);
+    // const h1 = window.document.getElementsByTagName('h1')[0];
+    // expect(h1.innerHTML).to.equal('Users');
+    done();
+    // window.close();
+  })
+})
